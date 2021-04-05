3e0fea17fd7a5d38e6cbff2e29b1773b4bf1a19e
Fukui-S <tibita13579@yahoo.co.jp>
2021-04-05 07:26:15 +0000

    [ADD]テスト実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,33 +3,33 @@ class BooksController < ApplicationController
       @books = Book.all
       @book = Book.new
    end
-   
+
    def show
       @book = Book.find(params[:id])
    end
-   
+
    def create
       @book = Book.new(book_params)
       if @book.save
-         redirect_to book_path
-         flash[:create] = 'Book was succesfully created.'
+         redirect_to book_path(@book.id)
+         flash[:create] = 'Book was successfully created.'
       else
          @books = Book.all
          render :index
       end
-      
+
    end
-     
+
    def edit
       @book = Book.find(params[:id])
    end
-   
+
    def destroy
       @book = Book.find(params[:id])
       @book.destroy
-      redirect_to book_path
+      redirect_to books_path
    end
-   
+
    def update
       @book = Book.find(params[:id])
       if @book.update(book_params)
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
          render :edit
       end
    end
-   
+
    private
    def book_params
       params.require(:book).permit(:title, :body)
